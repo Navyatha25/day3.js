2a3bf89c1428ae797f77cd7d9497975b2b175f2c
Navyatha <siddamnavya35@gmail.com>
2025-11-05 15:43:11 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Find the HTML element with id="message"
+let messageElement = document.getElementById("message");
+
+// Change its content
+messageElement.innerHTML = "Hello! This message is displayed using an external JavaScript file.";
